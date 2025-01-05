99cdc74289cdb227b99a839e916e5f8255f415f5
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-06 00:10:26 +0100

    chore: Update tracing dependencies and add tracing-serde

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,15 +1,32 @@
 use auth_service::application::http::{HttpServer, HttpServerConfig};
-use auth_service::env::Env;
+use auth_service::env::{AppEnv, Env};
 use auth_service::infrastructure::auth::firebase::authentication_service::AuthenticationServiceFirebase;
 use clap::Parser;
 use std::sync::Arc;
 
+fn init_logger(env: Arc<Env>) {
+    match env.env {
+        AppEnv::Development => {
+            tracing_subscriber::fmt::init();
+        }
+        AppEnv::Production => {
+            tracing_subscriber::fmt()
+            .json()
+            .with_max_level(tracing::Level::INFO)
+            .init();
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
-    tracing_subscriber::fmt::init();
-
     let env = Arc::new(Env::parse());
+
+    //tracing_subscriber::fmt::init();
+    init_logger(Arc::clone(&env));
+
+
 
     let authentication_service = Arc::new(AuthenticationServiceFirebase::new(
         env.firebase_api_key.clone(),
